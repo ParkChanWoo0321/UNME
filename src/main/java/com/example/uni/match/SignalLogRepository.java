b1131f2ea2b7dade068509800093a85e2596a3c1
ParkChanWoo0321 <op9563_@naver.com>
2025-09-20 02:58:56 +0900

    플러팅 학과 랭킹수정

--- src/main/java/com/example/uni/match/SignalLogRepository.java
+++ src/main/java/com/example/uni/match/SignalLogRepository.java
@@ -7,13 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface SignalLogRepository extends JpaRepository<SignalLog, Long> {
-    @Query("""
-           select l.receiverDepartment, count(l)
-           from SignalLog l
-           where l.receiverDepartment is not null
-             and l.receiverDepartment <> ''
-           group by l.receiverDepartment
-           order by count(l) desc
-           """)
+    @Query(value = """
+select receiver_department, count(*) as cnt
+from signal_logs
+where receiver_department is not null and receiver_department <> ''
+group by receiver_department
+order by cnt desc, receiver_department collate utf8mb4_0900_ai_ci asc
+""", nativeQuery = true)
     List<Object[]> countByReceiverDepartment();
 }
